3687b1498af36b9516d520f92eb05d8781dcbfea
Jérémie Astori <jeremie@astori.fr>
2017-03-04 17:30:17 -0500

    Link accounts and transactions together, specify account ID at transaction creation
    Also make it look a bit like SQL table format to make transition to a real storage easier.

--- backend/backend.scala
+++ backend/backend.scala
@@ -14,49 +14,37 @@ import DefaultJsonProtocol._
 
 // Leaving out date of transaction for now
 // http://www.javapractices.com/topic/TopicAction.do?Id=13
-case class Transaction(id: Int, /*date: DateTime,*/ description: String, amount: BigDecimal)
+case class Transaction(id: Int, /*date: DateTime,*/ description: String, amount: BigDecimal, accountId: Int)
 
-case class Account(id: Int, name: String, transactions: List[Transaction]) {
+case class Account(id: Int, name: String) {
+  def transactions: List[Transaction] = AccountRepo.transactions.filter(_.accountId == id)
   def total: BigDecimal = transactions.map(_.amount).sum
 }
 
 object AccountRepo {
   // Hardcoded list of accounts and transactions for now
   var accounts = List(
-    Account(1, "Checking Account", List(
-      Transaction(1, "MBTA", BigDecimal("22.5")),
-      Transaction(2, "Uber", BigDecimal("10.23"))
-    )),
-    Account(2, "Savings Account", List(
-      Transaction(3, "Interests", BigDecimal("0.34"))
-    ))
+    Account(1, "Checking Account"),
+    Account(2, "Savings Account")
   )
 
-  var transactions = Map(
-    1 -> Transaction(1, "MBTA", BigDecimal("22.5"))
+  var transactions = List(
+    Transaction(1, "MBTA", BigDecimal("22.5"), 1),
+    Transaction(2, "Uber", BigDecimal("10.23"), 1),
+    Transaction(3, "Interests", BigDecimal("0.34"), 2)
   )
 }
 
 class AccountRepo {
   def accounts: List[Account] = AccountRepo.accounts
   def account(id: Int): Option[Account] = accounts.find(_.id == id)
-  def transactions: List[Transaction] = AccountRepo.transactions.values.toList
-  def transaction(id: Int): Option[Transaction] = AccountRepo.transactions.get(id)
+  def transaction(id: Int): Option[Transaction] = AccountRepo.transactions.find(_.id == id)
 
   @GraphQLField
-  def createTransaction(description: String, amount: BigDecimal): Transaction = {
-    // Commenting out id generation until I find a way to get id-free input
-    val r = scala.util.Random
-    val id = r.nextInt(10000)
-    val transaction = Transaction(id, description, amount)
-
-    // For now, new transaction is added to first account. Later, an `accountId`
-    // argument will be given to the `createTransaction` mutation.
-    // AccountRepo.accounts = List(
-    //   accounts(0).copy(transactions = transaction :: accounts(0).transactions),
-    //   accounts(1)
-    // )
-    AccountRepo.transactions = AccountRepo.transactions + (id -> transaction)
+  def createTransaction(description: String, amount: BigDecimal, accountId: Int): Transaction = {
+    val id = scala.util.Random.nextInt(10000)
+    val transaction = Transaction(id, description, amount, accountId)
+    AccountRepo.transactions = transaction :: AccountRepo.transactions
     transaction
   }
 
@@ -67,7 +55,7 @@ class AccountRepo {
       amount = amount.getOrElse(t.amount)
     ))
     updatedTransaction.map((transaction: Transaction) =>
-      AccountRepo.transactions = AccountRepo.transactions + (id -> transaction)
+      transaction :: AccountRepo.transactions.filter(_.id != id)
     )
     updatedTransaction
   }
@@ -75,7 +63,7 @@ class AccountRepo {
   @GraphQLField
   def deleteTransaction(id: Int): Option[Transaction] = {
     val t = transaction(id)
-    AccountRepo.transactions = AccountRepo.transactions - id
+    AccountRepo.transactions = AccountRepo.transactions.filter(_.id != id)
     t
   }
 }
@@ -89,7 +77,7 @@ object AccountSchema {
 
   val AccountType = deriveObjectType[Unit, Account](
     ObjectTypeDescription("An account"),
-    IncludeMethods("total")
+    IncludeMethods("transactions", "total")
   )
 
   val QueryType = ObjectType("Query", fields[AccountRepo, Unit](
@@ -105,10 +93,6 @@ object AccountSchema {
     Field("transaction", OptionType(TransactionType),
       arguments = Id :: Nil,
       resolve  = c => c.ctx.transaction(c.arg(Id))
-    ),
-    Field("transactions", ListType(TransactionType),
-      description = Some("Returns a list of all transactions"),
-      resolve = _.ctx.transactions
     )
   ))
 
